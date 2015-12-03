af518dfaeb367c40f0ea5c02083b1d0ad8b2f418
Charlie Proctor <charlie@charlieproctor.com>
2015-12-03 17:50:51 -0500

    detect faces using haar cascade classifier

--- utils/ti_process.py
+++ utils/ti_process.py
@@ -7,6 +7,7 @@ if len(sys.argv) < 2:
 	sys.stderr.write("USAGE: python ti_process.py NUM_PROFILES\n")
 	sys.exit(os.EX_USAGE)
 
+HAAR = "../lib/opencv3/haarcascades/haarcascade_frontalface_default.xml"
 # connect to the database
 client = MongoClient()
 db = client.tinderx
@@ -25,8 +26,24 @@ for profile in profiles:
 	arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
 	img = cv2.imdecode(arr, cv2.IMREAD_COLOR)
 
-	# show the image (for one second)
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+	faceCascade = cv2.CascadeClassifier(HAAR)
+
+	faces = faceCascade.detectMultiScale(
+		gray,
+		scaleFactor=1.1,
+		minNeighbors=5,
+		minSize=(50, 50)
+		# flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+	)
+
+	# Draw a rectangle around the faces
+	for (x, y, w, h) in faces:
+		cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+	# show the image (for 1/2 second)
 	cv2.imshow(profile['name'].encode('ascii', 'ignore'),img)
-	cv2.waitKey(1000)
+	cv2.waitKey(500)
 
 cv2.destroyAllWindows()

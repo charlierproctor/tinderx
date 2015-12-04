cb436901072070dac2b915e4430717336e60f53d
Charlie Proctor <charlie@charlieproctor.com>
2015-12-04 02:28:11 -0500

    detect eyes

--- utils/ti_process.py
+++ utils/ti_process.py
@@ -7,7 +7,9 @@ if len(sys.argv) < 2:
 	sys.stderr.write("USAGE: python ti_process.py NUM_PROFILES\n")
 	sys.exit(os.EX_USAGE)
 
-HAAR = "../lib/opencv3/haarcascades/haarcascade_frontalface_default.xml"
+HAAR_EYES = "../lib/opencv3/haarcascades/haarcascade_eye.xml"
+HAAR_FACES = "../lib/opencv3/haarcascades/haarcascade_frontalface_default.xml"
+
 # connect to the database
 client = MongoClient()
 db = client.tinderx
@@ -28,19 +30,31 @@ for profile in profiles:
 
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-	faceCascade = cv2.CascadeClassifier(HAAR)
+	# detect and draw faces
+	faceCascade = cv2.CascadeClassifier(HAAR_FACES)
 
 	faces = faceCascade.detectMultiScale(
 		gray,
 		scaleFactor=1.1,
 		minNeighbors=5,
 		minSize=(50, 50)
-		# flags = cv2.cv.CV_HAAR_SCALE_IMAGE
 	)
 
-	# Draw a rectangle around the faces
 	for (x, y, w, h) in faces:
 		cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+	# detect and draw eyes
+	eyeCascade = cv2.CascadeClassifier(HAAR_EYES)
+
+	eyes = eyeCascade.detectMultiScale(
+		gray,
+		scaleFactor=1.1,
+		minNeighbors=5,
+		minSize=(10, 10)
+	)
+
+	for (x, y, w, h) in eyes:
+		cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
 	# show the image (for 1/2 second)
 	cv2.imshow(profile['name'].encode('ascii', 'ignore'),img)

8e52d82426d0c5aced66a9497a837ef38aaabc63
Charlie Proctor <charlie@charlieproctor.com>
2015-12-09 02:15:53 -0500

    post the facebook login results to /login

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -10,10 +10,16 @@ angular.module('tinderX.login', ['ui.router'])
 	})
 }])
 
-.controller('LoginCtrl', ['$scope', 'Facebook', function($scope, fb){
-	$scope.login = function() {
-		fb.login(function(res) {
-			console.log(res)
-		})
-	}
+.controller('LoginCtrl', ['$scope', 'Facebook', '$http', 
+	function($scope, fb, $http){
+		$scope.login = function() {
+			fb.login(function(res) {
+				$http.post('/login',res)
+				.success(function(data){
+					console.log(data)
+				}).error(function(data,status){
+					// TODO: do something on error. AND on facebook login error
+				})
+			})
+		}
 }])

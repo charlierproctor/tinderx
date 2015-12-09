ad7e6e572d025b2c5182bbcff9c3068663523115
Charlie Proctor <charlie@charlieproctor.com>
2015-12-09 02:27:05 -0500

    setting up / requests in flask

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 from core.tinderx import TinderX
 from core.user import User
 from core.db import Mongo
+
+from flask import Flask
+
+# configure the app
+app = Flask(__name__, static_folder='dist',static_url_path='')
+
+# send index.html on requests to /
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+# run the app
+if __name__ == '__main__':
+    app.run()
